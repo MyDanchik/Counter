05dc9fc431fb3e5b007ccd4a54320d1e33ec691d
Daniil <idanik.mega@gmail.com>
2024-01-13 16:12:49 +0300

    add UI

--- Counter-v1/Counter/CounterViewController.swift
+++ Counter-v1/Counter/CounterViewController.swift
@@ -5,25 +5,53 @@ class CounterViewController: UIViewController, CounterViewProtocol {
     
     let decreaseButton: UIButton = {
         let button = UIButton()
-        button.setTitle("Уменьшить", for: .normal)
-        button.setTitleColor(.blue, for: .normal)
+        button.setTitle("-", for: .normal)
+        button.tintColor = UIColor.white
+        button.backgroundColor = .continueButton
         button.translatesAutoresizingMaskIntoConstraints = false
+        button.heightAnchor.constraint(equalToConstant: 60).isActive = true
+        button.widthAnchor.constraint(equalToConstant: 190).isActive = true
+        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 25)
+        button.layer.cornerRadius = 10
         return button
     }()
     
     let increaseButton: UIButton = {
         let button = UIButton()
-        button.setTitle("Увеличить", for: .normal)
-        button.setTitleColor(.blue, for: .normal)
+        button.setTitle("+", for: .normal)
+        button.tintColor = UIColor.white
+        button.backgroundColor = .nextButton
         button.translatesAutoresizingMaskIntoConstraints = false
+        button.heightAnchor.constraint(equalToConstant: 60).isActive = true
+        button.widthAnchor.constraint(equalToConstant: 190).isActive = true
+        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 25)
+        button.layer.cornerRadius = 10
         return button
     }()
     
     let counterLabel: UILabel = {
         let label = UILabel()
         label.textAlignment = .center
-        label.font = UIFont.systemFont(ofSize: 20)
+        label.font = UIFont.systemFont(ofSize: 40)
+        label.textColor = .white
+        label.backgroundColor = .labelSet
         label.translatesAutoresizingMaskIntoConstraints = false
+        label.heightAnchor.constraint(equalToConstant: 80).isActive = true
+        label.widthAnchor.constraint(equalToConstant: 190).isActive = true
+        label.clipsToBounds = true
+        label.layer.cornerRadius = 10
+        return label
+    }()
+    
+    let textLabel: UILabel = {
+        let label = UILabel()
+        label.textAlignment = .center
+        label.text = "Счетчик"
+        label.font = UIFont.systemFont(ofSize: 60)
+        label.textColor = .white
+        label.backgroundColor = .clear
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.heightAnchor.constraint(equalToConstant: 80).isActive = true
         return label
     }()
     
@@ -38,22 +66,31 @@ class CounterViewController: UIViewController, CounterViewProtocol {
     }
     
     func setupUI() {
-        view.backgroundColor = .white
+        view.backgroundColor = .backgroundVC
         
         view.addSubview(decreaseButton)
         view.addSubview(increaseButton)
         view.addSubview(counterLabel)
+        view.addSubview(textLabel)
         
-        decreaseButton.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 20).isActive = true
-        decreaseButton.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
         decreaseButton.addTarget(self, action: #selector(decreaseButtonTapped), for: .touchUpInside)
         
-        increaseButton.topAnchor.constraint(equalTo: decreaseButton.bottomAnchor, constant: 20).isActive = true
-        increaseButton.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
         increaseButton.addTarget(self, action: #selector(increaseButtonTapped), for: .touchUpInside)
         
-        counterLabel.topAnchor.constraint(equalTo: increaseButton.bottomAnchor, constant: 20).isActive = true
-        counterLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        NSLayoutConstraint.activate([
+            decreaseButton.topAnchor.constraint(equalTo: increaseButton.bottomAnchor, constant: 20),
+            decreaseButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            
+            increaseButton.topAnchor.constraint(equalTo: counterLabel.bottomAnchor, constant: 20),
+            increaseButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            
+            counterLabel.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            counterLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            
+            textLabel.bottomAnchor.constraint(equalTo: counterLabel.topAnchor, constant: -50),
+            textLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            textLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20)
+        ])
     }
     
     @objc func decreaseButtonTapped() {
--- Counter-v1/ViewController.swift
+++ Counter-v1/ViewController.swift
@@ -1,19 +1,59 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    @IBOutlet weak var nextButton: UIButton!
-    @IBAction func nextButton(_ sender: UIButton) {
+    
+    let textLabel: UILabel = {
+        let label = UILabel()
+        label.textAlignment = .center
+        label.text = "Счетчик"
+        label.font = UIFont.systemFont(ofSize: 60)
+        label.textColor = .white
+        label.backgroundColor = .clear
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.heightAnchor.constraint(equalToConstant: 80).isActive = true
+        return label
+    }()
+    
+    let nextButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("Начать", for: .normal)
+        button.tintColor = UIColor.white
+        button.backgroundColor = .nextButton
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 18)
+        button.layer.cornerRadius = 10
+        return button
+    }()
+    
+    func setupUI() {
+        view.backgroundColor = .backgroundVC
+        view.addSubview(textLabel)
+        view.addSubview(nextButton)
+        
+        NSLayoutConstraint.activate([
+            nextButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            nextButton.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            nextButton.heightAnchor.constraint(equalToConstant: 60),
+            nextButton.widthAnchor.constraint(equalToConstant: 190),
+            
+            textLabel.bottomAnchor.constraint(equalTo: nextButton.topAnchor, constant: -50),
+            textLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            textLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20)
+        ])
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupUI()
+        nextButton.addTarget(self, action: #selector(buttonTapped), for: .touchUpInside)
+    }
+    
+    @objc func buttonTapped() {
+        print("Кнопка была нажата")
         let view = CounterViewController()
         let model = CounterModel()
         let presenter = CounterPresenter(view: view, model: model)
         view.presenter = presenter
         navigationController?.pushViewController(view, animated: true)
     }
-    override func viewDidLoad() {
-        super.viewDidLoad()
-    }
-
-
 }
-
