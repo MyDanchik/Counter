8ed8f43fc6dadc8261e0434e8d495448909e5d34
Daniil <idanik.mega@gmail.com>
2024-01-13 14:27:07 +0300

    add gitignore

---
+++ Counter-v1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Counter-v1
+//
+//  Created by Daniil Yushkevich on 13.01.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
